a90ca7706a01b626fb3bb34228d84c87e0725cf7
Victor Roman Archidona <vroman@loadfront.com>
2003-01-22 07:50:04 +0000

    Añadimos todo el "util" :)

---
+++ util/src/bmp2fnt/b2f.c
@@ -0,0 +1,160 @@
+/* 
+ * eDiv Compiler
+ * Copyleft (C) 2000-2002 Sion Entertainment
+ * http://www.sion-e.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _WIN32
+#error Este archivo aun NO ha sido portado a otras plataformas.
+#endif
+
+#include <windows.h>
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include <SDL/SDL.h>
+
+struct _fuente_control_s {
+	int offset_imagen ;
+	int size_imagen ;
+	int w , h , bytespp ;
+	SDL_Rect rect[256] ;
+	SDL_Surface *imagen ;
+};
+
+int main(int argc, char* argv[])
+{
+	FILE *fp, *out;
+	char tras[3], none[3];
+	char *pool, *px;
+	struct _fuente_control_s fuente_control_s;
+	SDL_Surface *imagen ;
+	int i, x, y;
+	fpos_t pos;
+	
+	out = fopen("stdout.txt", "w" );
+
+	imagen = SDL_LoadBMP("fuente.bmp");
+	SDL_LockSurface(imagen);
+        
+	// Se calculan los rects
+	px = imagen->pixels ;
+
+
+	// Color de borde ( El primero k se encuentra
+	none[0] = *(px + 0);
+	none[1] = *(px + 1);
+	none[2] = *(px + 2);
+
+	// Colo trasparente ( Fila 1, Columna1, corresponde al caracter 0 )
+	tras[0] = *(px + imagen->pitch + 3 + 0);
+	tras[1] = *(px + imagen->pitch + 3 + 1);
+	tras[2] = *(px + imagen->pitch + 3 + 2);
+
+
+	i = 1;
+	x = 3;
+	y = 0;
+	for ( x = 3 ; x < imagen->w ; x++ )
+	{
+		for ( y = 0 ; y < imagen->h ; y++ )
+		{
+			if (*(px + imagen->pitch*y + x*3 + 0) != none[0] ||
+				*(px + imagen->pitch*y + x*3 + 1) != none[1] ||
+				*(px + imagen->pitch*y + x*3 + 2) != none[2] )
+			{
+				fuente_control_s.rect[i].x = x ;
+				fuente_control_s.rect[i].y = y ;
+				for ( ; y < imagen->h ; y++ )
+				{
+					if (*(px + imagen->pitch*y + x*3 + 0) == none[0] &&
+						*(px + imagen->pitch*y + x*3 + 1) == none[1] &&
+						*(px + imagen->pitch*y + x*3 + 2) == none[2] )
+						break ;
+				}
+				y--;
+				fuente_control_s.rect[i].h = y - fuente_control_s.rect[i].y + 1;
+
+				for ( ; x < imagen->w ; x++ )
+				{
+					if (*(px + imagen->pitch*y + x*3 + 0) == none[0] &&
+						*(px + imagen->pitch*y + x*3 + 1) == none[1] &&
+						*(px + imagen->pitch*y + x*3 + 2) == none[2] )
+						break ;
+				}
+				x--;
+				fuente_control_s.rect[i].w = x - fuente_control_s.rect[i].x + 1;
+				x++;
+				
+				fprintf(out, "%i:  %i,%i-%i,%i\n" , 
+					i ,
+					fuente_control_s.rect[i].x ,
+					fuente_control_s.rect[i].y ,
+					fuente_control_s.rect[i].w ,
+					fuente_control_s.rect[i].h );
+					
+				i++;
+				break ;
+			}
+		}
+		if ( i > 255)
+			break ;
+	}
+
+	fp = fopen("fuente.fnt" , "w" );
+
+	fuente_control_s.offset_imagen = sizeof( fuente_control_s );
+	fuente_control_s.size_imagen = imagen->w * imagen->h * imagen->format->BytesPerPixel;
+	fuente_control_s.w = imagen->w ;
+	fuente_control_s.h = imagen->h ;
+	fuente_control_s.bytespp = imagen->format->BytesPerPixel;
+
+	printf("Sizeof control: %i\nOffset imagen: %i\nSizeof imagen: %i" ,
+		fuente_control_s.offset_imagen, fuente_control_s.offset_imagen ,
+		fuente_control_s.size_imagen );
+	getchar();
+
+	if(!(pool = malloc(fuente_control_s.size_imagen + 1) ) )
+		printf("ERROR: Memoria insuficiente\n");
+
+	for ( x = 0 ; x < imagen->w ; x++ )
+		for ( y = 0 ; y < imagen->h ; y++ )
+			for ( i = 0 ; i < imagen->format->BytesPerPixel ; i++ )
+				pool[ ( x + y * imagen->w ) * imagen->format->BytesPerPixel + i ] = *(px + ( y * imagen->pitch ) + ( x * imagen->format->BytesPerPixel ) + i );
+	
+	SDL_UnlockSurface( imagen );
+
+
+	printf("\nescribe: %i" , fwrite( &fuente_control_s , 1 , sizeof(fuente_control_s) , fp ) );
+
+	pos = fuente_control_s.offset_imagen;
+	fsetpos(fp , &pos );
+
+	printf( "\n%i\n" , ftell( fp ) );
+	getchar();
+	
+
+	fwrite( pool , 1 , fuente_control_s.size_imagen , fp );
+
+	free(pool);
+
+	exit (EXIT_SUCCESS);
+}
+
+
---
+++ util/src/iconchanger/iconchanger.c
@@ -0,0 +1,144 @@
+/* IconChanger
+ * Pequeña utilidad-complemento para eDIV que permite sustituir el icono del stub de
+ * win32 por otro. El icono nuevo debe ser exactamente igual que el original del stub,
+ * es decir, tiene que cumplir las siguientes condiciones:
+ * - Contener 6 imágenes en total:
+ *   + 16x16 a 4, 8 y 24 bits de color
+ *   + 32x32 a 4, 8 y 24 bits de color
+ * - "Pesar" exactamente 8.854 bytes.
+ * Este programa es muy simple así que no se molesta en comprobar el formato del fichero,
+ * únicamente comprueba su tamaño. Si metes un icono que no es adecuado.. allá tú..
+ * Puede ser útil en el futuro currarnos un programa decente que soporte cualquier tipo
+ * de iconos, que cambiara el tamaño del exe, etc etc, ya sería la reostia, pero por
+ * ahora con esto nos sobra.
+ * Pekeño inconveniente: sólo se puede cambiar el icono 1 vez :p  Si se kiere cambiar el
+ * icono por segunda vez, hay que recompilar el programa. En realidad no importa, ya que
+ * en verdad es lo que pasa en todos los compiladores...
+ * Si se kiere cambiar el icono y un virus te ha comido el ediv.exe o el .prg, pos tiras
+ * de Resource Hacker y a kaxkarla...
+ * 
+ * Er_Makina
+ */
+
+#ifndef _WIN32
+#error IconChanger es sólamente para Win32
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "iconchanger.h"
+
+
+#define ICO_SIZE	8854		// Tamaño que debe tener el .ico
+#define	ICO_START	0x66		// Offset donde se empieza a leer el .ico
+#define ICO_LEN		0x2230		// Tamaño del bloque a copiar del .ico al .exe
+
+
+void* e_malloc(size_t size)
+{
+	void* ret;
+
+	if(!(ret = malloc(size))) {
+		printf("ERROR: Memoria insuficiente.\n\n");
+		exit(1);
+	}
+
+	return ret;
+}
+
+int main(int argc, char* argv[])
+{
+	FILE* f;
+	unsigned char *exe,*ico;
+	int tamanyo;
+	int i,j;
+	int ok=0;
+
+	printf("eDIV IconChanger - Copyright (c) Sion Ltd. 2002\n\n");
+	if(argc!=3) {
+		printf("Modo de uso:\n\n    %s <programa.exe> <icono.ico>\n\n",argv[0]);
+		exit(2);
+	}
+
+	if((f=fopen(argv[2],"rb"))==NULL) {
+		printf("ERROR: No puedo abrir %s\n\n",argv[2]);
+		exit(3);
+	}
+
+	fseek(f,0,SEEK_END);
+	tamanyo=ftell(f);
+	if(tamanyo!=ICO_SIZE) {
+		fclose(f);
+		printf("ERROR: El icono no es del formato válido\n\n");
+		printf("Te refrescaré la memoria:\nEl icono (.ico) DEBE contener 6 imágenes en total, a saber:\n");
+		printf(" - 3 imágenes de 16x16 (a 4, 8 y 24 bits de color).\n");
+		printf(" - 3 imágenes de 32x32 (a 4, 8 y 24 bits de color).\n");
+		printf("No puede faltar ninguna, ya que el archivo debe ocupar EXACTAMENTE %d bytes.\n\n",ICO_SIZE);
+		exit(4);
+	}
+
+	ico=(unsigned char*)e_malloc(tamanyo);
+	fseek(f,0,SEEK_SET);
+	fread(ico,tamanyo,1,f);
+	fclose(f);
+
+	if((f=fopen(argv[1],"rb"))==NULL) {
+		printf("ERROR: No puedo abrir %s\n\n",argv[1]);
+		exit(5);
+	}
+
+	fseek(f,0,SEEK_END);
+	tamanyo=ftell(f);
+	fseek(f,0,SEEK_SET);
+	exe=(unsigned char*)e_malloc(tamanyo);	/* no creo ke pase nada por unos 125 kb...
+											   eso si, cuando se puedan meter el PAK y
+											   las DLL's en el exe, va a haber ke cambiar
+											   esto... */
+	fread(exe,tamanyo,1,f);
+	fclose(f);
+
+	// BUSKEDA
+
+	printf("Buscando sitio donde meter mano en el exe...\n");
+
+	for(i=20;i<tamanyo-sizeof(image);i++) {
+		if(exe[i]==image[0]) {
+			ok=1;
+			for(j=1;j<sizeof(image);j++) {
+				if(exe[i+j]!=image[j]) {
+					ok=0;
+					break;
+				}
+			}
+			if(ok) break;
+		}
+	}
+
+	if(!ok) {
+		free(ico);
+		free(exe);
+		printf("ERROR: El ejecutable no parece haber sido compilado con eDIV, o bien ya se le ha cambiado el icono.\n\n");
+		exit(6);
+	}
+
+	// "parcheamos" el exe en memoria
+	memcpy(&exe[i],&ico[ICO_START],ICO_LEN);
+	free(ico);
+	
+	// y escribimos el exe enterito al disco duro.. lo mio si es optimizar eh? xDD
+	if((f=fopen(argv[1],"wb"))==NULL) {
+		free(exe);
+		printf("ERROR: No tengo acceso de escritura a %s\n\n",argv[1]);
+		exit(7);
+	}
+
+	fwrite(exe,tamanyo,1,f);
+	fclose(f);
+	free(exe);
+	printf("Completado :)\n\n");
+
+	return 0;
+}
+
