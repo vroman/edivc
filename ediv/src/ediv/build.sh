a8409c31cf739232258bb822a9ebab9b383dfe01
Victor Roman Archidona <vroman@loadfront.com>
2014-07-23 12:33:22 +0200

    Quick'n'dirty way to compile edivc

---
+++ ediv/src/ediv/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CCOPTS="-I. -I../shared"
+
+FILES="
+edivcfg/dictionary
+edivcfg/e_error
+edivcfg/edivcfg
+edivcfg/mainparse
+edivcfg/strlib
+compiler
+ediv
+ediv_export
+encrypt
+expresion
+language
+listados
+lower
+ltlex
+modulos
+parser
+varindex
+../../src/shared/utils
+"
+
+OBJECTS=""
+
+for FILE in $FILES; do
+	gcc -c $FILE.c $CCOPTS -o $FILE.o
+	OBJECTS+="$FILE.o "
+done
+
+gcc $OBJECTS -o ../../bin/edivc -lz
+
+rm $OBJECTS
