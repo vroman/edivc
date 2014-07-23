91660ae8522134b2b1820a53fe7ebc28c6ff9f54
Victor Roman Archidona <vroman@loadfront.com>
2014-07-23 13:15:22 +0200

    Quick'n'dirty build script for the stub (interpreter)

---
+++ ediv/src/stub/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CCOPTS="-I. -I../shared -I/usr/local/include -DDBG"
+
+FILES="
+dll
+ediv_export
+error
+fatal
+inte
+language
+lower
+modulos
+stub
+varindex
+../shared/utils
+"
+
+OBJECTS=""
+
+for FILE in $FILES; do
+	gcc -c $FILE.c $CCOPTS -o $FILE.o
+	OBJECTS+="$FILE.o "
+done
+
+gcc $OBJECTS -o ../../bin/stub -lz
+
+rm $OBJECTS
